4d79b9a285c0320020d6e1db908d8de3ce96f50a
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-02-01 20:07:15 +0100

    Add solution for day06

--- Elixir/lib/day06/day06.ex
+++ Elixir/lib/day06/day06.ex
@@ -1,5 +1,6 @@
 defmodule Aoc2015.Day06 do
   import NimbleParsec
+  import Benchee
 
   order =
     choice([
@@ -71,7 +72,31 @@ defmodule Aoc2015.Day06 do
     |> Enum.count(&(&1 == true))
   end
 
-  def solution2(input) do
+  def solution1_2(input) do
+    input
+    |> Stream.map(fn x ->
+      {:ok, result, _, _, _, _} = instruction(x)
+      result
+    end)
+    |> Enum.reduce(%{}, fn [order, start_point, end_point], carte ->
+      {x_start, y_start} = start_point
+      {x_end, y_end} = end_point
+
+      for(
+        x <- x_start..x_end,
+        y <- y_start..y_end,
+        do: {x, y}
+      )
+      |> Enum.reduce(carte, fn {x, y}, carte ->
+        Map.update(carte, x, switch(%{}, y, order), fn line -> switch(line, y, order) end)
+      end)
+    end)
+    |> Map.values()
+    |> Enum.map(&(Map.values(&1) |> Enum.count(fn x -> x end)))
+    |> Enum.sum()
+  end
+
+  def solution2_1(input) do
     input
     |> Stream.map(fn x ->
       {:ok, result, _, _, _, _} = instruction(x)
@@ -92,15 +117,65 @@ defmodule Aoc2015.Day06 do
     |> Enum.sum()
   end
 
-  def part1 do
+  def solution2_2(input) do
+    input
+    |> Stream.map(fn x ->
+      {:ok, result, _, _, _, _} = instruction(x)
+      result
+    end)
+    |> Enum.reduce(%{}, fn [order, start_point, end_point], carte ->
+      {x_start, y_start} = start_point
+      {x_end, y_end} = end_point
+
+      for(
+        x <- x_start..x_end,
+        y <- y_start..y_end,
+        do: {x, y}
+      )
+      |> Enum.reduce(carte, fn {x, y}, carte ->
+        Map.update(carte, x, dim(%{}, y, order), fn line -> dim(line, y, order) end)
+      end)
+    end)
+    |> Map.values()
+    |> Enum.map(&(Map.values(&1) |> Enum.sum()))
+    |> Enum.sum()
+  end
+
+  def part1_1 do
     File.stream!("./lib/day06/day06.txt")
     |> solution1
     |> IO.inspect(label: "Day06 Part 1 result : ")
   end
 
-  def part2 do
+  def part1_2 do
     File.stream!("./lib/day06/day06.txt")
-    |> solution2
+    |> solution1_2
+    |> IO.inspect(label: "Day06 Part 1 result : ")
+  end
+
+  def part1 do
+    Benchee.run(%{
+      "Part1_1" => fn -> part1_1() end,
+      "Part1_2" => fn -> part1_2() end
+    })
+  end
+
+  def part2_1 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution2_1
     |> IO.inspect(label: "Day06 Part 2 result : ")
+  end
+
+  def part2_2 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution2_2
+    |> IO.inspect(label: "Day06 Part 2 result : ")
+  end
+
+  def part2 do
+    Benchee.run(%{
+      "Part2_1" => fn -> part2_1() end,
+      "Part2_2" => fn -> part2_2() end
+    })
   end
 end
