e4011343518ba5a79038866ab100baec4fab6f2d
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-02-22 21:05:31 +0100

    Refactor day06 + add better solutions

--- Elixir/lib/day06/day06.ex
+++ Elixir/lib/day06/day06.ex
@@ -109,38 +109,62 @@ defmodule Aoc2015.Day06 do
   end
 
   def solution1_4(input) do
-    light_map =
-      for x <- 0..999 do
-        %{
-          :x => x,
-          :line =>
-            for y <- 0..999 do
-              %{
-                :y => y,
-                :state => false
-              }
+    instructions = parse_instructions(input)
+
+    for x <- 0..999,
+        instructions_x =
+          Enum.filter(instructions, fn [_order, {x_start, _y_start}, {x_end, _y_end}] ->
+            x >= x_start and x <= x_end
+          end),
+        y <- 0..999,
+        instructions_y =
+          Enum.filter(instructions_x, fn [_order, {_x_start, y_start}, {_x_end, y_end}] ->
+            y >= y_start and y <= y_end
+          end),
+        reduce: 0 do
+      acc ->
+        Enum.reduce(instructions_y, 0, fn [order, _, _], acc_point ->
+          case order do
+            :turn_on -> 1
+            :turn_off -> 0
+            :toggle -> if acc_point == 1, do: 0, else: 1
+          end
+        end)
+        |> (&(&1 + acc)).()
+    end
+  end
+
+  def solution1_5(input) do
+    instructions = parse_instructions(input)
+
+    0..999
+    |> Enum.reduce(
+      0,
+      fn x, acc_x ->
+        instructions_x =
+          Enum.filter(instructions, fn [_order, {x_start, _y_start}, {x_end, _y_end}] ->
+            x >= x_start and x <= x_end
+          end)
+
+        0..999
+        |> Enum.reduce(0, fn y, acc_y ->
+          instructions_y =
+            Enum.filter(instructions_x, fn [_order, {_x_start, y_start}, {_x_end, y_end}] ->
+              y >= y_start and y <= y_end
+            end)
+
+          Enum.reduce(instructions_y, 0, fn [order, _, _], acc_point ->
+            case order do
+              :turn_on -> 1
+              :turn_off -> 0
+              :toggle -> if acc_point == 1, do: 0, else: 1
             end
-        }
+          end)
+          |> (&(&1 + acc_y)).()
+        end)
+        |> (&(&1 + acc_x)).()
       end
-
-    input
-    |> parse_instructions()
-    |> Enum.reduce(light_map, fn [order, {x_start, y_start}, {x_end, y_end}], carte ->
-      range = [
-        Access.filter(&(&1.x in x_start..x_end)),
-        :line,
-        Access.filter(&(&1.y in y_start..y_end)),
-        :state
-      ]
-
-      case order do
-        :turn_on -> put_in(carte, range, true)
-        :turn_off -> put_in(carte, range, false)
-        :toggle -> update_in(carte, range, &(not &1))
-      end
-    end)
-    |> get_in([Access.all(), :line, Access.all(), :state])
-    |> Enum.reduce(0, fn x, acc -> Enum.count(x, & &1) + acc end)
+    )
   end
 
   def solution2_1(input) do
@@ -172,6 +196,95 @@ defmodule Aoc2015.Day06 do
     end)
   end
 
+  def solution2_3(input) do
+    input
+    |> parse_instructions()
+    |> Enum.reduce(
+      %{},
+      fn [order, {x_start, y_start}, {x_end, y_end}], carte ->
+        for x <- x_start..x_end,
+            y <- y_start..y_end,
+            reduce: carte do
+          carte ->
+            case order do
+              :turn_on ->
+                update_in(carte, [Access.key(x, %{}), Access.key(y, 0)], &(&1 + 1))
+
+              :turn_off ->
+                update_in(
+                  carte,
+                  [Access.key(x, %{}), Access.key(y, 0)],
+                  &if(&1 == 0, do: 0, else: &1 - 1)
+                )
+
+              :toggle ->
+                update_in(carte, [Access.key(x, %{}), Access.key(y, 0)], &(&1 + 2))
+            end
+        end
+      end
+    )
+    |> Enum.reduce(0, fn {_, line}, acc ->
+      Enum.reduce(line, 0, fn {_, state}, acc_line -> state + acc_line end) + acc
+    end)
+  end
+
+  def solution2_4(input) do
+    instructions = parse_instructions(input)
+
+    for x <- 0..999,
+        instructions_x =
+          Enum.filter(instructions, fn [_order, {x_start, _y_start}, {x_end, _y_end}] ->
+            x >= x_start and x <= x_end
+          end),
+        y <- 0..999,
+        instructions_y =
+          Enum.filter(instructions_x, fn [_order, {_x_start, y_start}, {_x_end, y_end}] ->
+            y >= y_start and y <= y_end
+          end) do
+      Enum.reduce(instructions_y, 0, fn [order, _, _], acc_point ->
+        case order do
+          :turn_on -> acc_point + 1
+          :turn_off -> max(acc_point - 1, 0)
+          :toggle -> acc_point + 2
+        end
+      end)
+    end
+    |> Enum.sum()
+  end
+
+  def solution2_5(input) do
+    instructions = parse_instructions(input)
+
+    0..999
+    |> Enum.reduce(
+      0,
+      fn x, acc_x ->
+        instructions_x =
+          Enum.filter(instructions, fn [_order, {x_start, _y_start}, {x_end, _y_end}] ->
+            x >= x_start and x <= x_end
+          end)
+
+        0..999
+        |> Enum.reduce(0, fn y, acc_y ->
+          instructions_y =
+            Enum.filter(instructions_x, fn [_order, {_x_start, y_start}, {_x_end, y_end}] ->
+              y >= y_start and y <= y_end
+            end)
+
+          Enum.reduce(instructions_y, 0, fn [order, _, _], acc_point ->
+            case order do
+              :turn_on -> acc_point + 1
+              :turn_off -> max(acc_point - 1, 0)
+              :toggle -> acc_point + 2
+            end
+          end)
+          |> (&(&1 + acc_y)).()
+        end)
+        |> (&(&1 + acc_x)).()
+      end
+    )
+  end
+
   def part1_1 do
     File.stream!("./lib/day06/day06.txt")
     |> solution1
@@ -196,12 +309,19 @@ defmodule Aoc2015.Day06 do
     |> IO.inspect(label: "Day06 Part 1 result : ")
   end
 
+  def part1_5 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution1_5
+    |> IO.inspect(label: "Day06 Part 1 result : ")
+  end
+
   def part1 do
     Benchee.run(%{
       "Part1_1" => fn -> part1_1() end,
       "Part1_2" => fn -> part1_2() end,
       "Part1_3" => fn -> part1_3() end,
-      "Part1_4" => fn -> part1_4() end
+      "Part1_4" => fn -> part1_4() end,
+      "Part1_5" => fn -> part1_5() end
     })
   end
 
@@ -217,10 +337,31 @@ defmodule Aoc2015.Day06 do
     |> IO.inspect(label: "Day06 Part 2 result : ")
   end
 
+  def part2_3 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution2_3
+    |> IO.inspect(label: "Day06 Part 2 result : ")
+  end
+
+  def part2_4 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution2_4
+    |> IO.inspect(label: "Day06 Part 2 result : ")
+  end
+
+  def part2_5 do
+    File.stream!("./lib/day06/day06.txt")
+    |> solution2_5
+    |> IO.inspect(label: "Day06 Part 2 result : ")
+  end
+
   def part2 do
     Benchee.run(%{
       "Part2_1" => fn -> part2_1() end,
-      "Part2_2" => fn -> part2_2() end
+      "Part2_2" => fn -> part2_2() end,
+      "Part2_3" => fn -> part2_3() end,
+      "Part2_4" => fn -> part2_4() end,
+      "Part2_5" => fn -> part2_5() end
     })
   end
 end
