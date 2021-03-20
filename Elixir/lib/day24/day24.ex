dbd31593141eb181e8d92234c815be96cd354ae6
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-03-20 16:40:43 +0100

    Add Day24 - Part1

---
+++ Elixir/lib/day24/day24.ex
@@ -0,0 +1,58 @@
+defmodule Aoc2015.Day24 do
+  @day 24
+  @input_file "./lib/day#{@day}/day#{@day}.txt"
+
+  def parse(input) do
+    String.split(input, "\n", trim: true)
+    |> Enum.map(&String.to_integer(&1))
+    |> Enum.sort(:desc)
+  end
+
+  def add_package(groups, target_weight, packages) do
+    Enum.flat_map(
+      groups,
+      fn x ->
+        rest_weight = target_weight - Enum.sum(x)
+        possible_packages = Enum.filter(packages, &(&1 < Enum.min(x) && &1 <= rest_weight))
+        for package <- possible_packages, do: [package | x]
+      end
+    )
+    |> test_groups(target_weight, packages)
+  end
+
+  def test_groups(groups, target_weight, packages) do
+    groups_filtred = Enum.filter(groups, &(Enum.sum(&1) == target_weight))
+
+    if groups_filtred == [] do
+      add_package(Enum.filter(groups, &(Enum.sum(&1) < target_weight)), target_weight, packages)
+    else
+      groups_filtred
+    end
+  end
+
+  def solution1(input) do
+    packages = parse(input)
+    target_weight = Enum.sum(packages) / 3
+
+    Enum.map(packages, &[&1])
+    |> add_package(target_weight, packages)
+    |> Enum.map(fn x -> Enum.reduce(x, &(&1 * &2)) end)
+    |> Enum.min()
+  end
+
+  def solution2(input) do
+    input
+  end
+
+  def part1 do
+    File.read!(@input_file)
+    |> solution1
+    |> IO.inspect(label: "Day#{@day} Part 1 result ")
+  end
+
+  def part2 do
+    File.read!(@input_file)
+    |> solution2
+    |> IO.inspect(label: "Day#{@day} Part 2 result ")
+  end
+end
---
+++ Elixir/test/day24/day24_test.exs
@@ -0,0 +1,11 @@
+defmodule Aoc2015Test.Day24Test do
+  @day 24
+
+  use ExUnit.Case
+
+  test "Exemple Day#{@day} - Part 1" do
+  end
+
+  test "Exemple Day#{@day} - Part 2" do
+  end
+end
