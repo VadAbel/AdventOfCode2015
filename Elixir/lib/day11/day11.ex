e0e1665eb2d388b5a9325ade09a68bbf5d8d429e
VadAbel <22178832+VadAbel@users.noreply.github.com>
2021-08-08 11:49:47 +0200

    Refactor Day11 - Improve speed

--- Elixir/lib/day11/day11.ex
+++ Elixir/lib/day11/day11.ex
@@ -6,7 +6,7 @@ defmodule Aoc2015.Day11 do
     do: include_straight(password) && has_valid_char(password) && has_two_pair(password)
 
   def include_straight(password) when byte_size(password) < 3, do: false
-  def include_straight(<<x, y, z, _rest::binary>>) when z == y + 1 and y == x + 1, do: true
+  def include_straight(<<x, y, z, _rest::binary>>) when z == y - 1 and y == x - 1, do: true
   def include_straight(<<_x, y, z, rest::binary>>), do: include_straight(<<y, z, rest::binary>>)
 
   def has_valid_char(password),
@@ -18,36 +18,23 @@ defmodule Aoc2015.Day11 do
   defp count_pair(<<x, y, rest::binary>>) when x == y, do: 1 + count_pair(rest)
   defp count_pair(<<_x, y, rest::binary>>), do: count_pair(<<y, rest::binary>>)
 
-  def next_valid_password(password) do
-    next_password = increment_password(password)
+  defp increment_password(<<x, rest::binary>>) when x == ?z,
+    do: <<?a>> <> increment_password(rest)
 
-    case is_valid_password(next_password) do
-      true -> next_password
-      _ -> next_valid_password(next_password)
-    end
-  end
-
-  defp increment_password(password) do
-    password
-    |> String.reverse()
-    |> increment_reverse_password()
-    |> String.reverse()
-  end
-
-  defp increment_reverse_password(<<x, rest::binary>>) when x == ?z,
-    do: <<?a>> <> increment_reverse_password(rest)
-
-  defp increment_reverse_password(<<x, rest::binary>>), do: <<x + 1, rest::binary>>
+  defp increment_password(<<x, rest::binary>>), do: <<x + 1, rest::binary>>
 
   def solution1(input) do
-    input
-    |> next_valid_password()
+    Stream.iterate(input |> String.reverse(), &increment_password(&1))
+    |> Stream.filter(&is_valid_password(&1))
+    |> Enum.at(0)
+    |> String.reverse()
   end
 
   def solution2(input) do
-    input
-    |> next_valid_password()
-    |> next_valid_password()
+    Stream.iterate(input |> String.reverse(), &increment_password(&1))
+    |> Stream.filter(&is_valid_password(&1))
+    |> Enum.at(1)
+    |> String.reverse()
   end
 
   def part1 do
